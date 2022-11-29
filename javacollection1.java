e1fb471c998829022c7bbb65dc4f98f07ce3da85
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-11-30 01:14:02 +0530

    Add files via upload

---
+++ javacollection1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class javacollection1 {
+
+    public static void main(String[] args) {
+        Collection<Integer> a = new ArrayList<Integer>();
+        a.add(1);
+        System.out.println(a);
+    }
+    
+}
