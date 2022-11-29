fbe1c626b29abc1d4bb03c7e974eac3e82a0dcaf
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-11-30 01:25:49 +0530

    Add files via upload

---
+++ javacollection2.java
@@ -0,0 +1,12 @@
+import java.util.Collection;
+import java.util.HashSet;
+class javacollection2 {
+
+    public static void main(String[] args) {
+        Collection<Integer> a = new HashSet<Integer>();
+        a.add(1);
+        System.out.println(a);
+    }
+    
+    
+}
