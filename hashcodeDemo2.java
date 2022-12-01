86acf9590d3a2e1cc3ed70e1738f13474a1cc461
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-01 05:58:59 +0530

    Add files via upload

---
+++ hashcodeDemo2.java
@@ -0,0 +1,14 @@
+public class hashcodeDemo2 {
+
+    public static void main(String[] args) {
+        String i = "A";
+        String j = "B";
+        String k = "C";
+
+        System.out.println(i.hashCode());
+        System.out.println(j.hashCode());
+        System.out.println(k.hashCode());
+
+    }
+    
+}
