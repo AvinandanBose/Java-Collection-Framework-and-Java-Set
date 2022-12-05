0b0e02604ac71ff5f512a8cbdf0fe8530b192846
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-05 16:09:06 +0530

    Add files via upload

--- HashSetDemo15g.java
+++ HashSetDemo15g.java
@@ -1,4 +1,6 @@
 import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class HashSetDemo15g {
@@ -10,6 +12,11 @@ public class HashSetDemo15g {
         set1.add(2);
         set1.add(3);
         set1.add(4);
+        set1.add(6);
+        set1.add(7);
+        set1.add(8);
+        set1.add(9);
+        set1.add(10);
 
         set2.add("A");
         set2.add("a");
@@ -22,10 +29,18 @@ public class HashSetDemo15g {
         boolean c = set2.stream().dropWhile(s -> s.length() == 1).noneMatch(s -> s.length() == 1);
         boolean d = set2.stream().dropWhile(s -> s == "A").anyMatch(s -> s== "A");
         
-
+        Set<Integer> set3 = set1.stream().dropWhile(s -> (s==1)).collect(Collectors.toSet());
+        HashSet<Integer>set4 = set1.stream().dropWhile(s -> ((s / 2)==1)).collect(Collectors.toCollection(HashSet::new));
+        HashSet<Integer> set5 = set1.stream().dropWhile(s -> (s == 1))
+                .collect(HashSet::new, HashSet::add, HashSet::addAll);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
+        System.out.println(set3);
+        System.out.println(set4);
+        System.out.println(set5);
+
+        
         
         
     }  
