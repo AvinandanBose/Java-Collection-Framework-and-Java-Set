d0b724b0b2cc6c01604e484e9816e839902b70a9
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-06 15:57:24 +0530

    Add files via upload

---
+++ TreeSetDemo3.java
@@ -0,0 +1,17 @@
+package TreeSet;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+public class TreeSetDemo3 {
+    public static void main(String[] args) {
+        SortedSet<String> set = new TreeSet<String>();
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        set.add("D");
+        set.add("E");
+        set.add("F");
+        System.out.println(set);
+    }
+}
+  
