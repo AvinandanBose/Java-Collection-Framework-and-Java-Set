9f3d8913b2a9c5ac2d2f3cc888a4021885320b25
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-07 22:25:50 +0530

    Add files via upload

---
+++ TreeSetDemo16.java
@@ -0,0 +1,28 @@
+package TreeSet;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo16 {
+    public static void main(String[] args) {
+        TreeSet<Float> ts = new TreeSet<>();
+        TreeSet<Float> ts1 = new TreeSet<>();
+        ts.add(10.58f);
+        ts.add(20.78f);
+        ts.add(30.65f);
+        ts.add(10.47f);
+        ts.add(50.98f);
+        ts.add(60.65f);
+
+        ts1.add(10.55f);
+        ts1.add(25.88f);
+        ts1.add(34.65f);
+        ts1.add(45.78f);
+        ts1.add(57.98f);
+        ts1.add(99.65f);
+
+        System.out.println(ts.pollFirst());
+        System.out.println(ts);
+        System.out.println(ts1.pollLast());
+        System.out.println(ts1);                                   
+    }
+}
