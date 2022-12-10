34ff1e6d4ef38b20b694c393f193642283bc51f2
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-10 17:46:24 +0530

    Update HashSetDemo24.java

--- HashSetDemo24.java
+++ HashSetDemo24.java
@@ -11,10 +11,13 @@ public class HashSetDemo24 {
         set.add("b");
 
          set.hashCode();
+        //this is from : java.util.AbstractSet.hashCode()
 
         System.out.println(set.hashCode());
 
         set.forEach(i -> System.out.println(i.hashCode()) );
+        //Note : i.hashCode() is of Wrapper Class String's Hash Code and not of Abstract Set.
+        //i.e. : java.lang.String.hashCode()
     }
 
 
