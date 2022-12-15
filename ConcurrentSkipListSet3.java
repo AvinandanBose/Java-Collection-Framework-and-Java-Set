95ac45a9b55943e0f5e8fd600b09f7bbc2108d62
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-15 17:10:42 +0530

    Add files via upload

---
+++ ConcurrentSkipListSet3.java
@@ -0,0 +1,51 @@
+package ConcurrentSkipListSet;
+
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.Set;
+public class ConcurrentSkipListSet3 {
+    public static void main(String[] args) {
+        Set<Float> set = new ConcurrentSkipListSet<>();
+
+        set.add(1.09f);
+        set.add(2.89f);
+        set.add(3.90f);
+        set.add(4.98f);
+
+        // addAll
+        Set<Float> set1 = new ConcurrentSkipListSet<>();
+        set1.addAll(set);
+        System.out.println(set1);
+
+        // containsAll
+        Boolean b = set.containsAll(set1);
+        System.out.println(b);
+
+        // retainAll
+
+        set1.retainAll(set);
+        System.out.println(set1);
+
+        // toArray
+        Object[] arr = set1.toArray();
+        for (Object o : arr) {
+            System.out.println(o);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        // toArray(T[] a)
+        Float[] arr1 = new Float[set1.size()];
+        set1.toArray(arr1);
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+
+        // hashCode
+        System.out.println(set1.hashCode());
+
+    }
+
+    
+}
